35bd3532f654db7632a090eabae80b931a1153dc
Nathan <nathanjsim42@gmail.com>
2022-04-22 16:03:54 +0200

    test

---
+++ push.sh
@@ -0,0 +1,53 @@
+if (( $# != 1 )); then
+  echo "Invalid push arguments!"
+  exit 1
+fi
+
+cd Libft/Leaky
+if (( $? != 0 )); then
+  echo "You must be in the root directory of the project to push!"
+  exit 1
+fi
+
+git status | grep -q "Leaky"
+if (( $? == 0 )); then
+  echo "Changes were made to Leaky, please commit them manually"
+  exit 1
+fi
+
+git status | grep -q "Your branch is ahead of"
+if (( $? == 0 )); then
+  echo "Please push your local commits made in Leaky manually"
+  exit 1
+fi
+
+cd ..
+git status | grep -q "Libft"
+if (( $? == 0 )); then
+  echo "Changes were made to Libft, please commit them manually"
+  exit 1
+fi
+
+git status | grep -q "Your branch is ahead of"
+if (( $? == 0 )); then
+  echo "Please push your local commits made in Libft manually"
+  exit 1
+fi
+
+git status | grep -q "Changes staged for commit"
+if (( $? != 0 )); then
+  echo "Nothing was added to be commited"
+  exit 1
+fi
+
+git commit -m $1
+if (( $? != 0 )); then
+  echo "Could not commit the changes"
+  exit 1
+fi
+
+git push
+if (( $? != 0 )); then
+  echo "Could not push changes"
+  exit 1
+fi
