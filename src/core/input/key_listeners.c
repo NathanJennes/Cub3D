3bd5e66980cc92cb41a0e4e1379791641c56bff7
Nathan <nathanjsim42@gmail.com>
2022-05-31 15:56:22 +0200

    Hitting tab returns to menu

--- src/core/input/key_listeners.c
+++ src/core/input/key_listeners.c
@@ -6,7 +6,7 @@
 /*   By: njennes <njennes@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 15:42:50 by njennes           #+#    #+#             */
-/*   Updated: 2022/05/26 16:33:31 by njennes          ###   ########.fr       */
+/*   Updated: 2022/05/31 15:53:16 by njennes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,13 @@ int	key_pressed_listener(int keycode)
 	t_mlx	*app;
 
 	app = get_app();
-	if (keycode == KEY_LEFT || keycode == KEY_UP || keycode == KEY_RIGHT
-		|| keycode == KEY_DOWN)
-	{
-	}
 	if (keycode == KEY_ESC)
 		close_app();
-	if (keycode == KEY_F3)
+	else if (keycode == KEY_F3)
 		switch_debug_ui();
-	if (keycode == KEY_RETURN)
+	else if (keycode == KEY_RETURN)
 		app->game_state = IN_GAME;
+	else if (keycode == KEY_TAB)
+		app->game_state = MENU;
 	return (0);
 }
