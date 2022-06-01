2f1d8c04fdf69950cd3003240cb5efa113d9a997
Nathan <nathanjsim42@gmail.com>
2022-06-01 19:02:16 +0200

    Moved game rendering into a function to clean-up main_loop

--- src/core/main_loop.c
+++ src/core/main_loop.c
@@ -6,7 +6,7 @@
 /*   By: njennes <njennes@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 15:44:35 by njennes           #+#    #+#             */
-/*   Updated: 2022/06/01 18:34:17 by njennes          ###   ########.fr       */
+/*   Updated: 2022/06/01 18:59:01 by njennes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 #include "render.h"
 
 void	render_test_scene(const t_mlx *app);
+
+static void 	render_game(t_mlx *app, const t_settings *settings, struct timeval time[4]);
 
 int	main_loop(void)
 {
@@ -27,23 +29,7 @@ int	main_loop(void)
 	settings = get_settings();
 	update_player(&app->gamestate.player);
 	if (app->app_state & IN_GAME)
-	{
-		gettimeofday(&time[0], NULL);
-		draw_rect_unsafe(ivec2(0, 0), ivec2(settings->win_w, settings->halfw_h), CEILLING);
-		draw_rect_unsafe(ivec2(0, settings->halfw_h), ivec2(settings->win_w, settings->halfw_h), FLOOR);
-		mlx_put_image_to_window(app->mlx, app->win, app->frame.img, 0, 0);
-		gettimeofday(&time[1], NULL);
-		renderer_render();
-		gettimeofday(&time[2], NULL);
-		render_test_scene(app);
-		gettimeofday(&time[3], NULL);
-		if (app->ui.debug_ui == TRUE)
-			printf("[FRAME - RENDER]: background: %lldms, walls: %lldms, test_scene: %lldms, total %lldms\n",
-				(int64_t)((time[1].tv_sec * 1000 + time[1].tv_usec / 1000) - (time[0].tv_sec * 1000 + time[0].tv_usec / 1000)),
-				(int64_t)((time[2].tv_sec * 1000 + time[2].tv_usec / 1000) - (time[1].tv_sec * 1000 + time[1].tv_usec / 1000)),
-				(int64_t)((time[3].tv_sec * 1000 + time[3].tv_usec / 1000) - (time[2].tv_sec * 1000 + time[2].tv_usec / 1000)),
-				(int64_t)((time[3].tv_sec * 1000 + time[3].tv_usec / 1000) - (time[0].tv_sec * 1000 + time[0].tv_usec / 1000)));
-	}
+		render_game(app, settings, time);
 	else
 		clear_screen(BKGD_COLOR);
 	if (app->ui.debug_ui == TRUE)
@@ -53,4 +39,23 @@ int	main_loop(void)
 	update_ui();
 	render_ui();
 	return (0);
+}
+
+static void render_game(t_mlx *app, const t_settings *settings, struct timeval time[4])
+{
+	gettimeofday(&time[0], NULL);
+	draw_rect_unsafe(ivec2(0, 0), ivec2(settings->win_w, settings->halfw_h), CEILLING);
+	draw_rect_unsafe(ivec2(0, settings->halfw_h), ivec2(settings->win_w, settings->halfw_h), FLOOR);
+	mlx_put_image_to_window(app->mlx, app->win, app->frame.img, 0, 0);
+	gettimeofday(&time[1], NULL);
+	renderer_render();
+	gettimeofday(&time[2], NULL);
+	render_test_scene(app);
+	gettimeofday(&time[3], NULL);
+	if (app->ui.debug_ui == TRUE)
+		printf("[FRAME - RENDER]: background: %lldms, walls: %lldms, test_scene: %lldms, total %lldms\n",
+			(int64_t)((time[1].tv_sec * 1000 + time[1].tv_usec / 1000) - (time[0].tv_sec * 1000 + time[0].tv_usec / 1000)),
+			(int64_t)((time[2].tv_sec * 1000 + time[2].tv_usec / 1000) - (time[1].tv_sec * 1000 + time[1].tv_usec / 1000)),
+			(int64_t)((time[3].tv_sec * 1000 + time[3].tv_usec / 1000) - (time[2].tv_sec * 1000 + time[2].tv_usec / 1000)),
+			(int64_t)((time[3].tv_sec * 1000 + time[3].tv_usec / 1000) - (time[0].tv_sec * 1000 + time[0].tv_usec / 1000)));
 }
